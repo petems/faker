d4809410707f9949a7981a8887ce8444b08beb8b
petems <p.morsou@gmail.com>
2013-09-25 21:03:22 +0100

    Blah

--- lib/faker.rb
+++ lib/faker.rb
@@ -91,7 +91,7 @@ module Faker
       # into method calls that can be used to generate a
       # formatted translation: e.g., "#{first_name} #{last_name}".
       def parse(key)
-        fetch(key).scan(/(\(?)#\{([A-Za-z]+\.)?([^\}]+)\}([^#]+)?/).map {|prefix, kls, meth, etc|
+        return_string = fetch(key).scan(/(\(?)#\{([A-Za-z]+\.)?([^\}]+)\}([^#]+)?/).map {|prefix, kls, meth, etc|
           # If the token had a class Prefix (e.g., Name.first_name)
           # grab the constant, otherwise use self
           cls = kls ? Faker.const_get(kls.chop) : self
@@ -107,6 +107,12 @@ module Faker
           # And tack on spaces, commas, etc. left over in the string
           text += etc.to_s
         }.join
+
+        puts "Returning: #{return_string}"
+
+        return_string
+
+
       end
 
       # Call I18n.translate with our configured locale if no
